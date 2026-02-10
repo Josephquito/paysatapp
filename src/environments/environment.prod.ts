1fe512d61d1e87983a288ec548599ffaa05c8f02
Josephquito <jdquito.3@sudamericano.edu.ec>
2026-02-10 10:56:21 -0500

    final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   chatApiUrl: 'http://localhost:3001/chat',
-  formApiUrl: 'https://paysatcontact-api.onrender.com',
+  formApiUrl: 'https://web-contactform-api.onrender.com',
 };
