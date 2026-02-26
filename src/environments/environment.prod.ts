a642f866533a3d4b8260965d94f3de6b14bd1953
PAYSAT MONEY <apppayment27@gmail.com>
2026-02-26 07:56:18 -0500

    Update chatApiUrl for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  chatApiUrl: 'http://localhost:3001/chat',
+  chatApiUrl: 'https://mz-chatbot-paysat-2026.onrender.com/chat',
   formApiUrl: 'https://web-contactform-api.onrender.com',
 };
